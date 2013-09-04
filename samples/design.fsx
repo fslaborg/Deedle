586c1d00edd5756be9dff80a7801b984745d8812
tpetricek <tomas@tomasp.net>
2013-09-04 19:24:39 -0400

    Optimize lookup, add module-based API, add aggregation and sample

---
+++ samples/design.fsx
@@ -0,0 +1,116 @@
+﻿(**
+
+F# Data Frame
+=============
+
+This is the first prototype of F# Data Frame library that was discussed here earlier. 
+For more information, please have a look at the following document:
+
+ * [Structured Data Library for F#/C#](https://docs.google.com/document/d/1M_hQinAQQrxYm7Ajn7yj38vpnMj32WfEp372OLG00rU/edit)
+
+The current prototype implements only basic functionality, but it (hopefully) provides
+the right "core" internals that should make it easier to add all the additional 
+(useful) features. Here are a few notes about the desing:
+
+### Library internals
+
+The following types are (mostly) not directly visible to the user, but you could
+use them when extending the library: 
+
+ * `IVector<'TAddress, 'TValue>` represents a vector (essentially an abstract data
+   storage) that contains values `'TValue` that can be accessed via an address
+   `'TAddress`. A simple concrete implementation is an array with `int` addresses,
+   but we aim to make this abstract - one could use an array of arrays with `int64`
+   index for large data sets, lazy vector that loads data from a stream or even 
+   a virtual vector with e.g. Cassandra data source). 
+   
+   An important thing about vectors is that they handle missing values, so vector 
+   of integers is actually more like `array<option<int>>` (but we have a custom value 
+   type so that this is continuous block of memory). We decided that handling missing
+   values is something that is so important for data frame, that it should be directly
+   supported rather than done by e.g. storing optional or nullable values. Our 
+   implementation actually does a simple optimization - if there are no missing values,
+   it just stores `array<int>`.
+
+ * `VectorConstruction<'TAddress>` is a discriminated union (DSL) that describes
+   construction of vector. For every vector type, there is an `IVectorBuilder<'TAddress>`
+   that knows how to construct vectors using the construction instructions (these 
+   include things like re-shuffling of elements, appending vectors, getting a sub-range
+   etc.)
+
+ * `IIndex<'TKey, 'TAddress>` represents an index - that is, a mapping from keys
+   of a series or data frame to addresses in a vector. In the simple case, this is just
+   a hash table that returns the `int` offset in an array when given a key (e.g.
+   `string` or `DateTime`). A super-simple index would just map `int` offsets to 
+   `int` addresses via an identity function (not implemented yet!) - if you have
+   series or data frame that is simply a list of recrods.
+
+Now, the following types are directly used:
+
+ * `Series<'TKey, 'TValue>` represents a series of values `'TValue` indexed by an
+   index `'TKey`. Although this is not quite fully implemented yet, a series uses
+   an abstract vector, index and vector builder, so it should work with any data
+   representation. A series provides some standard slicing operators, projection, 
+   filtering etc. There are also some binary operators (multiply by a scalar, add series, etc.)
+
+ * `Frame<'TRowKey, 'TColumnKey>` represents a data frame with rows indexed using
+   `TRowKey` (this could be `DateTime` or just ordinal numbers like `int`) and columns
+   indexed by `TColumnKey` (typically a `string`). The data in the frame can be
+   hetrogeneous (e.g. different types of values in different columns) and so accessing
+   data is dynamic - but you can e.g. get a typed series.
+   
+   The operations available on the data frame include adding & removing series (which 
+   aligns the new series according to the row index), joins (again - aligns the series) 
+   etc. You can also get all rows as a series of (column) series and all columns as a 
+   series of (row) series.
+
+### Questions & call to action
+
+As mentioned earlier, the current version is just a prototype. We're hoping that the 
+design of the internals is now reasonable, but the end user API is missing most of
+the important functions. So:
+
+ * **Send comments & samples** - if you have some interesting problem that might be
+   a good fit for data frames, then please share a sample or problem description so
+   that we can make sure that we support all you might need. We plan to share the code
+   publicly in ~1 week so you can submit pull requests then too! Also, if you have
+   some particularly nice Scala/Python/R/Matlab code that you'd like to do in F#,
+   share it too so that we can copy clever ideas :-).
+
+ * **Time series vs. pivot table** - there is some mismatch between two possible
+   interpretations and uses of the library. One is for time-series data (e.g. in finance)
+   where one typically works with dates as row indices. More generally, you can see this
+   as _continous_ index. It makes sense to do interpolation, sort the observations,
+   align them, re-scale them etc.
+   
+   The other case is when we have some _discrete_ observations (perhaps a list of 
+   records with customer data, a list of prices of different stock prices etc.) In this
+   case, we need more "pivot table" functions etc.
+
+   Although these two uses are quite different, we feel that it might make sense to use
+   the same type for both (just with a different index). The problem is that this might
+   make the API more complex. Although, if we can keep the distincion in the type, we can
+   use F# 3.1 extension methods that extend just "discrete data frame" or "continous data 
+   frame". Also, F# functions could be structured in two modules like `df |> Discrete.groupBy`
+   and `df |> Continuous.interoplateMissingValues`.
+
+ * **Immutability** - in the current version, a series is immutable data type, but a
+   data frame supports limited mutation - you can add new series, drop a series & replace
+   a series (but you cannot mutate the series). This seems to be useful because it works
+   nicely with the `?<-` operator and you do not have to re-bind when you're writing some
+   research script.
+
+ * **Type provider** - we are thinking about using type providers to give some additional
+   safety (like checking column names and types in a data frame). This is currently
+   on the TODO list - I think we can do something useful here, although it will 
+   certainly be limited. 
+
+   The current idea is that you migth want to do some research/prototyping using a 
+   dynamic data frame, but once you're done and have some more stable data, you should
+   be able to write, say `DataFrame<"Open:float,Close:float">(dynamicDf)` and get a
+   new typed data frame. 
+
+If you have any comments regarding the topics above, please discuss them on the mailing
+list - this is currently a prototype and we are certainly open to changing things.   
+
+*)
---
+++ samples/volatility.fsx
@@ -0,0 +1,71 @@
+﻿(**
+
+Volatility
+==========
+
+*)
+#I "../bin"
+#I "../lib"
+#I "../packages"
+#r "MathNet.Numerics.dll"
+#load "FSharp.DataFrame.fsx"
+#load "FSharp.Charting.fsx"
+
+open System
+open FSharp.DataFrame
+open FSharp.Charting
+open MathNet.Numerics.Distributions
+
+// Generate price using geometric Brownian motion
+let randomPrice drift volatility initial count = 
+  let dist = Normal(0.0, 1.0, RandomSource=Random(0))  
+  let dt = 1.0 / (250.0 * 24.0 * 60.0)
+  let driftExp = (drift - 0.5 * pown volatility 2) * dt
+  let randExp = volatility * (sqrt dt)
+  (DateTimeOffset(DateTime(2013, 1, 1)), initial) |> Seq.unfold (fun (dt, price) ->
+    let price = price * exp (driftExp + randExp * dist.Sample()) 
+    let dt = dt.AddSeconds(1.0)
+    Some((dt, price), (dt, price))) |> Seq.take count
+
+// Generate two "high-frequency" time series (with different volatility)
+let hfq1 = Series.ofValues (randomPrice 0.05 0.1 20.0 (24*60*60))
+let hfq2 = Series.ofValues (randomPrice 0.05 0.2 20.0 (24*60*60))
+
+// Chart them using F# Chart to see what they look like
+Chart.Combine(
+  [ Chart.FastLine(hfq1.Observations)
+    Chart.FastLine(hfq2.Observations) ]).WithYAxis(Min=17.0, Max=22.0)
+  
+// Calculate the means of the two series (and see that they are the same)
+hfq1 |> Series.mean
+hfq1 |> Series.mean
+
+// Get all day data in 1 minute intervals
+let intervals = [ for i in 0.0 .. 24.0*60.0 - 1.0 -> DateTimeOffset(DateTime(2013, 1, 1)).AddMinutes(i) ]
+
+// Get nearest smaller values for the specified interval & calculate logs
+// Then take difference between previous and the next log value
+let logs1 = hfq1 |> Series.lookupAll intervals LookupSemantics.NearestGreater |> log
+let diffs = logs1 |> Series.pairwiseWith (fun _ (v1, v2) -> v2 - v1)
+
+Chart.Rows 
+  [ Chart.Line(logs1.Observations);
+    Chart.Line(diffs.Observations) ]
+
+// Get 1 hour chunks and build a data frame with one column
+// for each hour (containing 60 rows for 60 minutes in the hour)
+let chunkedLogs =
+  diffs 
+  |> Series.chunk (TimeSpan(1, 0, 0))
+  |> Series.map (fun _ -> Series.withOrdinalIndex)
+  |> Frame.ofColumns
+
+
+// Means and standard deviations for each hour
+let sdvs = chunkedLogs |> Frame.sdv
+let means = chunkedLogs |> Frame.mean
+
+// Display a chart that shows means and sdvs per hour of day
+Chart.Rows
+  [ Chart.Column(sdvs.Observations)
+    Chart.Column(means.Observations) ]
