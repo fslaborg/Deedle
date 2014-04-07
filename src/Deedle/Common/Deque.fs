ec42fe9c7c3f753a666882faa6c82f216520ed57
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-04-07 21:24:37 +0200

    Cleanup

--- src/Deedle/Common/Deque.fs
+++ src/Deedle/Common/Deque.fs
@@ -7,143 +7,115 @@ open System.Diagnostics
 open System.Runtime.InteropServices
 
 [<SerializableAttribute>]
-type Deque<'T>(initCapacity : int) =
+type Deque<'T>(initCapacity : int) = 
     let mutable capacity = initCapacity
     // The circular array holding the items.
     let mutable buffer : 'T array = Array.zeroCreate capacity
-    
     // The first element offset from the beginning of the data array.
     let mutable startOffset = 0
-
     let mutable count = 0
     let mutable capacityClosestPowerOfTwoMinusOne = 0
-
-    let copyArray size =
+    
+    let copyArray size = 
         let newArray = Array.zeroCreate size
-
-        if 0 <> startOffset && startOffset + count >= capacity then
+        if 0 <> startOffset && startOffset + count >= capacity then 
             let lengthFromStart = capacity - startOffset
-            let lengthFromEnd = count - lengthFromStart            
-
+            let lengthFromEnd = count - lengthFromStart
             Array.Copy(buffer, startOffset, newArray, 0, lengthFromStart)
-
             Array.Copy(buffer, 0, newArray, lengthFromStart, lengthFromEnd)
-        else
-            Array.Copy(buffer, startOffset, newArray, 0, count)
+        else Array.Copy(buffer, startOffset, newArray, 0, count)
         newArray
-
+    
     /// Sets the total number of elements the internal array can hold without resizing.
-    let doubleCapacity() =
-        if capacity < 0 then
-            raise <| new ArgumentOutOfRangeException("value", "Capacity is less than 0.")
-
-        if capacity < count then
+    let doubleCapacity() = 
+        if capacity < 0 then raise <| new ArgumentOutOfRangeException("value", "Capacity is less than 0.")
+        if capacity < count then 
             raise <| new InvalidOperationException("Capacity cannot be set to a value less than Count")
-
-        if capacity <= buffer.Length then () else
-
-        // Set up to use the new buffer.
-        capacity <- capacity * 2
-        buffer <- copyArray(capacity)
-        capacityClosestPowerOfTwoMinusOne <- capacity - 1
-        startOffset <- 0
-
-
+        if capacity <= buffer.Length then ()
+        else 
+            // Set up to use the new buffer.
+            capacity <- capacity * 2
+            buffer <- copyArray (capacity)
+            capacityClosestPowerOfTwoMinusOne <- capacity - 1
+            startOffset <- 0
+    
     let toBufferIndex index = (index + startOffset) &&& capacityClosestPowerOfTwoMinusOne
-
-    let iterate() =
-        seq {
-            if startOffset + count > capacity then
-            
-                for i in startOffset..capacity-1 do
+    
+    let iterate() = 
+        seq { 
+            if startOffset + count > capacity then 
+                for i in startOffset..capacity - 1 do
                     yield buffer.[i]
-
-                let endIndex = toBufferIndex(count - startOffset - 1)
-
-                for i in 0..endIndex-1 do
-                    yield buffer.[i]           
-            else            
+                let endIndex = toBufferIndex (count - startOffset - 1)
+                for i in 0..endIndex - 1 do
+                    yield buffer.[i]
+            else 
                 let endIndex = startOffset + count
-                for i in startOffset .. endIndex-1 do
+                for i in startOffset..endIndex - 1 do
                     yield buffer.[i]
         }
-
+    
     /// Creates a new Deque with the default capacity
     new() = Deque<'T>(16)
-
+    
     /// Gets the total number of elements the internal array can hold without resizing.
     member this.Capacity = buffer.Length
-
+    
     /// Gets the number of elements contained in the Deque.
     member this.Count = count
-
-    member private this.shiftStartOffset(value) =
-        startOffset <- toBufferIndex(value)
+    
+    member private this.shiftStartOffset (value) = 
+        startOffset <- toBufferIndex (value)
         startOffset
-
-    member private this.preShiftStartOffset(value) =
+    
+    member private this.preShiftStartOffset (value) = 
         let offset = startOffset
-        this.shiftStartOffset(value) |> ignore
+        this.shiftStartOffset (value) |> ignore
         offset
-
+    
     /// Gets whether or not the Deque is filled to capacity.
     member this.IsFull = this.Count >= this.Capacity
-
+    
     /// Gets whether or not the Deque is empty.
     member this.IsEmpty = this.Count = 0
-
-
+    
     /// Adds the provided item to the back of the Deque.
-    member this.Add(item) =
-        if this.Count >= this.Capacity then
-            doubleCapacity()
-
-        buffer.[toBufferIndex(startOffset + this.Count)] <- item
+    member this.Add(item) = 
+        if this.Count >= this.Capacity then doubleCapacity()
+        buffer.[toBufferIndex (startOffset + this.Count)] <- item
         count <- count + 1
-
+    
     /// Removes an item from the front of the Deque and returns it.
-    member this.RemoveFirst() =
-        if this.IsEmpty then
-            raise <| new InvalidOperationException("The Deque is empty")
-
-        let item = buffer.[this.preShiftStartOffset(1)]
+    member this.RemoveFirst() = 
+        if this.IsEmpty then raise <| new InvalidOperationException("The Deque is empty")
+        let item = buffer.[this.preShiftStartOffset (1)]
         count <- count - 1
         item
-
+    
     /// Removes an item from the back of the Deque and returns it.
-    member this.RemoveLast() =
-        if this.IsEmpty then
-            raise <| new InvalidOperationException("The Deque is empty")
-
+    member this.RemoveLast() = 
+        if this.IsEmpty then raise <| new InvalidOperationException("The Deque is empty")
         count <- count - 1
-        buffer.[toBufferIndex(this.Count)]
-
+        buffer.[toBufferIndex (this.Count)]
+    
     /// Gets the value at the specified index of the Deque
-    member this.Get(index) =
-        if index >= this.Count then
+    member this.Get(index) = 
+        if index >= this.Count then 
             raise <| new IndexOutOfRangeException("The supplied index is greater than the Count")
-
-        buffer.[toBufferIndex(index)]
-
+        buffer.[toBufferIndex (index)]
+    
     /// Gets the element at the front
-    member this.First =
-        if count = 0 then
-            raise <| new InvalidOperationException("The Deque is empty")
-
-        buffer.[toBufferIndex(0)]
-
+    member this.First = 
+        if count = 0 then raise <| new InvalidOperationException("The Deque is empty")
+        buffer.[toBufferIndex (0)]
+    
     /// Gets the element at the end
-    member this.Last =
-        if count = 0 then
-            raise <| new InvalidOperationException("The Deque is empty")
-
-        buffer.[toBufferIndex(count - 1)]
-
+    member this.Last = 
+        if count = 0 then raise <| new InvalidOperationException("The Deque is empty")
+        buffer.[toBufferIndex (count - 1)]
+    
     interface System.Collections.Generic.IEnumerable<'T> with
-        member this.GetEnumerator() =
-          iterate().GetEnumerator()
-
+        member this.GetEnumerator() = iterate().GetEnumerator()
+    
     interface System.Collections.IEnumerable with
-        member this.GetEnumerator () =
-          (iterate().GetEnumerator())
-            :> System.Collections.IEnumerator        
+        member this.GetEnumerator() = (iterate().GetEnumerator()) :> System.Collections.IEnumerator
