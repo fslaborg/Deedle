27b0cdb05967c222a373b3c2f1eeb0b35175a7a9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-04-07 17:59:10 +0200

    Use our own Deque - references #195

---
+++ src/Deedle/Deque.fs
@@ -0,0 +1,172 @@
+﻿/// A generic Deque class. It can be thought of as a double-ended queue, hence Deque. This allows for an O(1) AddFront, AddBack, RemoveFront, RemoveBack.
+/// The Deque also has O(1) indexed lookup, as it is backed by a circular array.
+module Deedle.Deque
+
+open System
+
+/// The default capacity of the deque.
+[<Literal>]
+let defaultCapacity = 16
+
+let inline closestPowerOfTwoGreaterThan x =
+   let x = x - 1
+   let x = x ||| (x >>> 1)
+   let x = x ||| (x >>> 2)
+   let x = x ||| (x >>> 4)
+   let x = x ||| (x >>> 8)
+   let x = x ||| (x >>> 16)
+   x+1
+
+type Deque<'T>(capacity : int) =
+    // The circular array holding the items.
+    let mutable buffer : 'T array = 
+        [||]
+    
+    // The first element offset from the beginning of the data array.
+    let mutable startOffset = 0
+
+    let mutable count = 0
+    let mutable capacityClosestPowerOfTwoMinusOne = 0
+
+    /// Creates a new Deque with the default capacity
+    new() = Deque<'T>(defaultCapacity)
+
+    /// Gets the total number of elements the internal array can hold without resizing.
+    member this.Capacity = buffer.Length
+
+    /// Gets the number of elements contained in the Deque.
+    member this.Count = count
+
+    member private this.CopyArray(size) =
+        let newArray = Array.create size Unchecked.defaultof<'T>
+
+        if 0 <> startOffset && startOffset + this.Count >= this.Capacity then
+            let lengthFromStart = this.Capacity - startOffset
+            let lengthFromEnd = this.Count - lengthFromStart            
+
+            Array.Copy(buffer, startOffset, newArray, 0, lengthFromStart)
+
+            Array.Copy(buffer, 0, newArray, lengthFromStart, lengthFromEnd)
+        else
+            Array.Copy(buffer, startOffset, newArray, 0, this.Count)
+        newArray
+
+    /// Sets the total number of elements the internal array can hold without resizing.
+    member this.SetCapacity(capacity : int) =
+        if capacity < 0 then
+            raise <| new ArgumentOutOfRangeException("value", "Capacity is less than 0.")
+
+        if capacity < this.Count then
+            raise <| new InvalidOperationException("Capacity cannot be set to a value less than Count")
+
+        if capacity = buffer.Length then () else
+
+        // Create a new array and copy the old values.
+        let powOfTwo = closestPowerOfTwoGreaterThan capacity
+
+        // Set up to use the new buffer.
+        buffer <- this.CopyArray(powOfTwo)
+        capacityClosestPowerOfTwoMinusOne <- powOfTwo - 1
+        startOffset <- 0
+
+    member private this.ensureCapacityFor(numElements) =
+        if this.Count + numElements > this.Capacity then
+            this.SetCapacity(this.Count + numElements)
+
+    member private this.shiftStartOffset(value) =
+        startOffset <- this.toBufferIndex(value)
+        startOffset
+
+    member private this.preShiftStartOffset(value) =
+        let offset = startOffset
+        this.shiftStartOffset(value) |> ignore
+        offset
+
+    /// Gets whether or not the Deque is filled to capacity.
+    member this.IsFull = this.Count >= this.Capacity
+
+    /// Gets whether or not the Deque is empty.
+    member this.IsEmpty = this.Count = 0
+
+    member this.toBufferIndex index =
+        (index + startOffset) &&& capacityClosestPowerOfTwoMinusOne
+
+    member this.iterate() =
+        // The below is done for performance reasons.
+        // Rather than doing bounds checking and modulo arithmetic
+        // that would go along with calls to Get(index), we can skip
+        // all of that by referencing the underlying array.
+        seq {
+            if startOffset + this.Count > this.Capacity then
+            
+                for i in startOffset..this.Capacity-1 do
+                    yield buffer.[i]
+
+                let endIndex = this.toBufferIndex(this.Count - startOffset - 1)
+
+                for i in 0..endIndex-1 do
+                    yield buffer.[i]           
+            else            
+                let endIndex = startOffset + this.Count
+                for i in startOffset .. endIndex-1 do
+                    yield buffer.[i]
+        }
+
+    /// Adds the provided item to the back of the Deque.
+    member this.AddBack(item) =
+        this.ensureCapacityFor(1)
+        buffer.[this.toBufferIndex(startOffset + this.Count)] <- item
+        count <- count + 1
+
+    /// Adds the provided item to the back of the Deque.
+    member this.Add(item) =
+        this.ensureCapacityFor(1)
+        buffer.[this.toBufferIndex(startOffset + this.Count)] <- item
+        count <- count + 1
+
+    /// Removes an item from the front of the Deque and returns it.
+    member this.RemoveFront() =
+        if this.IsEmpty then
+            raise <| new InvalidOperationException("The Deque is empty")
+
+        let item = buffer.[this.preShiftStartOffset(1)]
+        count <- count - 1
+        item
+
+    /// Removes an item from the back of the Deque and returns it.
+    member this.RemoveBack() =
+        if this.IsEmpty then
+            raise <| new InvalidOperationException("The Deque is empty")
+
+        count <- count - 1
+        buffer.[this.toBufferIndex(this.Count)]
+
+    /// Gets the value at the specified index of the Deque
+    member this.Get(index) =
+        if index >= this.Count then
+            raise <| new IndexOutOfRangeException("The supplied index is greater than the Count")
+
+        buffer.[this.toBufferIndex(index)]
+
+    /// Gets the element at the front
+    member this.First =
+        if count = 0 then
+            raise <| new InvalidOperationException("The Deque is empty")
+
+        buffer.[this.toBufferIndex(0)]
+
+    /// Gets the element at the end
+    member this.Last =
+        if count = 0 then
+            raise <| new InvalidOperationException("The Deque is empty")
+
+        buffer.[this.toBufferIndex(count - 1)]
+
+    interface System.Collections.Generic.IEnumerable<'T> with
+        member this.GetEnumerator() =
+          this.iterate().GetEnumerator()
+
+    interface System.Collections.IEnumerable with
+        member this.GetEnumerator () =
+          (this.iterate().GetEnumerator())
+            :> System.Collections.IEnumerator        
---
+++ tests/Deedle.Tests/Deque.fs
@@ -0,0 +1,102 @@
+﻿#if INTERACTIVE
+#load "../../bin/Deedle.fsx"
+#r "../../packages/NUnit.2.6.3/lib/nunit.framework.dll"
+#r "../../packages/FsCheck.0.9.1.0/lib/net40-Client/FsCheck.dll"
+#r "../../packages/MathNet.Numerics.3.0.0-alpha8/lib/net40/MathNet.Numerics.dll"
+#load "../Common/FsUnit.fs"
+#else
+module Deedle.Tests.Deque
+#endif
+
+open System
+open System.Linq
+open System.Collections.Generic
+open System.Globalization
+open FsUnit
+open FsCheck
+open NUnit.Framework
+
+open Deedle
+
+[<Test>]
+let ``empty deque is empty`` () =
+  let d = Deque.Deque()
+
+  d.IsEmpty |> shouldEqual true
+  
+[<Test>]
+let ``can add single element`` () =
+  let d = Deque.Deque()
+
+  d.Add(1)
+  
+  d.IsEmpty |> shouldEqual false 
+  d.Count |> shouldEqual 1
+
+[<Test>]
+let ``can add elements`` () =
+  let d = Deque.Deque()
+
+  d.Add(1)
+  d.Add(1)
+  d.Add(2)
+  d.Add(3)
+  d.Add(4)
+  
+  d.IsEmpty |> shouldEqual false 
+  d.Count |> shouldEqual 5
+
+[<Test>]
+let ``can add elements and remove from front`` () =
+  let d = Deque.Deque()
+
+  for x in 1..2049 do
+      d.Add(x.ToString())
+ 
+  d.IsEmpty |> shouldEqual false 
+  d.Count |> shouldEqual 2049
+
+  for x in 1..2049 do
+      d.RemoveFront() |> shouldEqual (x.ToString())
+
+  d.IsEmpty |> shouldEqual true
+  d.Count |> shouldEqual 0
+
+
+[<Test>]
+let ``can add elements and remove from back`` () =
+  let d = Deque.Deque()
+
+  for x in 1..2049 do
+      d.Add(x)
+ 
+  d.IsEmpty |> shouldEqual false 
+  d.Count |> shouldEqual 2049
+
+  for x in 1..2049 do
+      d.RemoveBack() |> shouldEqual (2049-x+1)
+
+  d.IsEmpty |> shouldEqual true
+  d.Count |> shouldEqual 0
+
+
+[<Test>]
+let ``can add elements and interate`` () =
+  let d = Deque.Deque()
+
+  for x in 1..2049 do
+      d.Add(x)
+ 
+  let l = ref 1
+  for x in d do
+    x |> shouldEqual !l
+    l := x + 1
+
+[<Test>]
+let ``can add elements and convert to list`` () =
+  let d = Deque.Deque()
+
+  for x in 1..2049 do
+      d.Add(x)
+ 
+  d |> Seq.toList |> shouldEqual [1..2049]
