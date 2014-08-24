b7e0c47902e2473c20f350441549cce169ef914b
Tomas Petricek <tomas@tomasp.net>
2014-08-24 04:11:14 +0200

    Recover documentation tests

--- tests/Deedle.Documentation.Tests/Documentation.Tests.fs
+++ tests/Deedle.Documentation.Tests/Documentation.Tests.fs
@@ -1,7 +1,20 @@
 ﻿// --------------------------------------------------------------------------------------
 // Test that the documentation is generated correctly withtout F# errors 
 // --------------------------------------------------------------------------------------
-namespace FSharp.Data.Tests.Documentation
+#if INTERACTIVE
+#I "../../packages/FSharp.Formatting.2.4.21/lib/net40"
+#I "../../packages/RazorEngine.3.3.0/lib/net40/"
+#r "../../packages/Microsoft.AspNet.Razor.2.0.30506.0/lib/net40/System.Web.Razor.dll"
+#r "../../packages/FSharp.Compiler.Service.0.0.59/lib/net40/FSharp.Compiler.Service.dll"
+#r "RazorEngine.dll"
+#r "FSharp.Literate.dll"
+#r "FSharp.CodeFormat.dll"
+#r "FSharp.MetadataFormat.dll"
+#r "../../packages/NUnit.2.6.3/lib/nunit.framework.dll"
+#load "../Common/FsUnit.fs"
+#else
+module FSharp.Data.Tests.DocumentationTests
+#endif
 
 open FsUnit
 open NUnit.Framework
@@ -12,62 +25,60 @@ open System.Reflection
 open FSharp.Literate
 open FSharp.CodeFormat
 
-module DocumentationTests = 
+// Initialization of the test - lookup the documentation files,
+// create temp folder for the output and load the F# compiler DLL
+let (@@) a b = Path.Combine(a, b)
+let template = __SOURCE_DIRECTORY__ @@ "../../tools/template.html"
+let sources = __SOURCE_DIRECTORY__ @@ "../../docs/content"
+let output = Path.GetTempPath() @@ "Deedle.Docs"
+if Directory.Exists(output) then Directory.Delete(output, true)
+do Directory.CreateDirectory(output) |> ignore
 
-  //alow tests that access the network to work when you're behind a proxy
-  WebRequest.DefaultWebProxy.Credentials <- CredentialCache.DefaultNetworkCredentials
 
-  // Initialization of the test - lookup the documentation files,
-  // create temp folder for the output and load the F# compiler DLL
-  let template = Path.Combine(__SOURCE_DIRECTORY__, "../../tools/template.html")
-  let sources = Path.Combine(__SOURCE_DIRECTORY__, "../../samples")
-  let output = Path.Combine(Path.GetTempPath(), "FSharp.Data.Docs")
-  if Directory.Exists(output) then Directory.Delete(output, true)
-  Directory.CreateDirectory(output) |> ignore
+/// Process a specified file in the documentation folder and return 
+/// the total number of unexpected errors found (print them to the output too)
+let processFile file =
+  printfn "Processing '%s'" file
 
-  // Lookup compiler DLL (Note: This requires F# 3.1!)
-  let locations = 
-    [ "%ProgramFiles%\\Microsoft SDKs\\F#\\3.1\\Framework\\v4.0\\FSharp.Compiler.dll"
-      "%ProgramFiles(x86)%\\Microsoft SDKs\\F#\\3.1\\Framework\\v4.0\\FSharp.Compiler.dll" ]
-  let compiler = 
-    try
-      locations |> Seq.pick (fun location ->
-        try 
-          let location = Environment.ExpandEnvironmentVariables(location)
-          if not (File.Exists(location)) then None else
-            Some(Assembly.LoadFile(Environment.ExpandEnvironmentVariables(location)))
-        with _ -> None)
-    with _ -> failwith "F# DataFrame documentation requires F# 3.1!"
+  let dir = Path.GetDirectoryName(Path.Combine(output, file))
+  if not (Directory.Exists(dir)) then Directory.CreateDirectory(dir) |> ignore
 
-  /// Process a specified file in the documentation folder and return 
-  /// the total number of unexpected errors found (print them to the output too)
-  let processFile file =
-    printfn "Processing '%s'" file
-    let errorCount = ref 0
+  let evaluationErrors = ResizeArray()
+#if INTERACTIVE
+  let fsiEvaluator = FsiEvaluator()
+  fsiEvaluator.EvaluationFailed |> Event.add evaluationErrors.Add
+  let literateDoc = Literate.ParseScriptFile(Path.Combine(sources, file), fsiEvaluator = fsiEvaluator)
+#else
+  let literateDoc = Literate.ParseScriptFile(Path.Combine(sources, file)) 
+#endif
+  Seq.append
+    (literateDoc.Errors 
+     |> Seq.choose (fun (SourceError(startl, endl, kind, msg)) ->
+       if msg <> "Multiple references to 'mscorlib.dll' are not permitted" then
+         Some <| sprintf "%A %s (%s)" (startl, endl) msg file
+       else None))
+    (evaluationErrors |> Seq.map (fun x -> x.ToString()))
+  |> String.concat "\n"
 
-    let errorHandler(file, SourceError(startl, endl, kind, msg)) = 
-      if msg <> "Multiple references to 'mscorlib.dll' are not permitted" then
-        printfn "%A %s (%s)" (startl, endl) msg file
-        incr errorCount
+// ------------------------------------------------------------------------------------
+// Core API documentation
+// ------------------------------------------------------------------------------------
 
-    let dir = Path.GetDirectoryName(Path.Combine(output, file))
-    if not (Directory.Exists(dir)) then Directory.CreateDirectory(dir) |> ignore
+let docFiles = 
+  seq { for sub in [ "."; ] do
+          for file in Directory.EnumerateFiles(sources @@ sub, "*.fsx") do
+            yield sub + "/" + Path.GetFileName(file) }
 
-    Literate.ProcessScriptFile
-      ( Path.Combine(sources, file), template, Path.Combine(output, file), 
-        errorHandler = errorHandler,
-        fsharpCompiler = compiler )
-    errorCount.Value
+#if INTERACTIVE
+for file in docFiles do 
+    printfn "%s" (processFile file)
+#else
 
-  // ------------------------------------------------------------------------------------
-  // Core API documentation
+[<Test>]
+[<TestCaseSource "docFiles">]
+let ``Documentation generated correctly `` file = 
+  let errors = processFile file
+  if errors <> "" then
+    Assert.Fail("Found errors when processing file '" + file + "':\n" + errors)
 
-  let docFiles = 
-    seq { for sub in [ "." ] do
-            for file in Directory.EnumerateFiles(Path.Combine(sources, sub), "*.fsx") do
-              yield sub + "/" + Path.GetFileName(file) }
-  
-  [<Test>]
-  [<TestCaseSource "docFiles">]
-  let ``Documentation generated correctly `` file = 
-    Assert.AreEqual(0, processFile file)
+#endif
