934f2bcad63e856110e4b9d0c59b2227e4652b82
tpetricek <tomas@tomasp.net>
2015-06-18 01:02:32 +0200

    Update version number and release'

--- src/Deedle/Common/AssemblyInfo.fs
+++ src/Deedle/Common/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Deedle")>]
 [<assembly: AssemblyProductAttribute("Deedle")>]
 [<assembly: AssemblyDescriptionAttribute("Easy to use .NET library for data manipulation and scientific programming")>]
-[<assembly: AssemblyVersionAttribute("1.1.5")>]
-[<assembly: AssemblyFileVersionAttribute("1.1.5")>]
+[<assembly: AssemblyVersionAttribute("1.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.1.5"
+    let [<Literal>] Version = "1.2.0"
