360d5645730addebcc73780979592c9584960970
Ildhesten <noneofyour@business.com>
2020-04-05 16:53:09 +0200

    Plugged in linear regression and pca logic, shamelessly from Deedle.Extras

---
+++ src/Deedle.Math/LinearRegression.fs
@@ -0,0 +1,253 @@
+﻿namespace Deedle.Math
+
+module rec LinearRegression =
+  open Deedle
+  open MathNet.Numerics
+
+
+  module Fit =
+    /// <summary>
+    /// Represents a linear model fitted to specific values in a data frame.
+    /// </summary>
+    type t<'a,'b> when 'a : equality and 'b : equality =
+      {
+        InputFrame : Frame<'a,'b>
+        Coefficients : Series<'b, float>
+        FitIntercept : 'b option
+        yKey : 'b
+      }
+
+    /// <summary>
+    /// Construct a new fit.
+    /// </summary>
+    /// <param name="input">The data frame that served as input for the regression.</param>
+    /// <param name="coeffs">The coefficients found by the linear regression</param>
+    /// <param name="yKey">The y column used</param>
+    /// <param name="fitIntercept">An option that specifies whether the intercept should be fitted.</param>
+    let make input coeffs yKey fitIntercept =
+      {
+        InputFrame = input
+        Coefficients = coeffs
+        FitIntercept = fitIntercept
+        yKey = yKey
+      }
+
+    let private computeXtXinverse (fit : t<'a,'b>) =
+      let addInterceptColumn (frame : Frame<'a,'b>) =
+        match fit.FitIntercept with
+        | None -> frame
+        | Some columnKey ->
+            let interceptSeries =
+              frame.RowKeys
+              |> Seq.map (fun rowKey -> (rowKey, 1.0))
+              |> Series.ofObservations
+            frame
+            |> Frame.addCol columnKey interceptSeries
+      let computeXtXInverse (frame : Frame<'a,'b>) =
+        let columnKeys = frame.ColumnKeys
+        let matrix = MathNet.Numerics.LinearAlgebra.Matrix.Build.DenseOfArray(Frame.toArray2D frame)
+        (matrix.Transpose() * matrix).Inverse().ToArray()
+        |> Frame.ofArray2D
+        |> Frame.mapColKeys (fun i -> Seq.item i columnKeys)
+        |> Frame.mapRowKeys (fun i -> Seq.item i columnKeys)
+      fit.InputFrame
+      |> Frame.filterCols (fun columnKey _ -> fit.Coefficients.ContainsKey(columnKey))
+      |> addInterceptColumn
+      |> computeXtXInverse
+
+    /// <summary>
+    /// The dataframe used for fitting the data.
+    /// </summary>
+    /// <param name="fit">The fit.</param>
+    let input fit =
+      fit.InputFrame
+
+    /// <summary>
+    /// The coefficients found by the linear regression.
+    /// </summary>
+    /// <param name="fit">The fit.</param>
+    let coefficients fit =
+      fit.Coefficients
+
+    /// <summary>
+    /// Whether or not the fit includes an estimate of the intercept.
+    /// </summary>
+    /// <param name="fit">The fit.</param>
+    let fitIntercept fit =
+      fit.FitIntercept
+
+    /// <summary>
+    /// Computes the fitted values from the linear model.
+    /// </summary>
+    /// <param name="fit">The fit.</param>
+    let fittedValues fit =
+      let fitIntercept = fit.FitIntercept
+      let df = fit.InputFrame
+      let coefficients = fit.Coefficients
+      // TODO: Investigate whether we can simplify the code to pointwise multiply series.
+      let fitRow (series : ObjectSeries<'b>) =
+        match fitIntercept with
+        | None -> let terms = coefficients |> Series.map (fun k v -> v * series.GetAs<float>(k))
+                  terms.Sum()
+        | Some interceptKey ->
+                  let terms =
+                    coefficients
+                    |> Series.filter (fun k _ -> k<> interceptKey)
+                    |> Series.map (fun k v -> v * series.GetAs<float>(k))
+                  terms.Sum() + coefficients.[interceptKey]
+      df |> Frame.mapRowValues fitRow
+
+    /// <summary>
+    /// Computes the residuals of the regression (y - yHat)
+    /// </summary>
+    /// <param name="fit">The fit.</param>
+    let residuals fit =
+      let fitted = fittedValues fit
+      fit.InputFrame.[fit.yKey] - fitted
+
+    let yKey fit =
+      fit.yKey
+
+    module Summary =
+      open System
+
+      type t<'b> when 'b : equality =
+        {
+          LinearFormula : string
+          ResidualFiveVals : Series<string,float>
+          TTable : Frame<'b,string>
+          RSquared : float
+          AdjRSquared : float
+        }
+
+        override x.ToString () =
+          let newLine = Environment.NewLine
+          let formattedFiveVals = Frame.ofRows ["", x.ResidualFiveVals]
+          (sprintf "Formula: %s" x.LinearFormula) + newLine +
+          (formattedFiveVals.Format()) + newLine +
+          (x.TTable.Format()) + newLine +
+          (sprintf "R^2: %f, Adj. R^2: %f" x.RSquared x.AdjRSquared) + newLine
+
+
+    let private computeToleranceFor xKey xKeys frame =
+      let otherKeys = xKeys |> Series.keys |> Seq.except (Seq.singleton xKey)
+      let fitXKey = multiDim otherKeys xKey None frame
+      let actual = Frame.getCol xKey frame |> Series.values |> Seq.toArray
+      let fitted = Fit.fittedValues fitXKey |> Series.values |> Seq.toArray
+      1.0 - (GoodnessOfFit.RSquared (fitted, actual))
+
+    let private computeSquaredErrorFor (stdErr : float) xKeys (frame : Frame<'a,'b>) xKey =
+      let otherKeys = xKeys |> Series.keys |> Seq.except (Seq.singleton xKey)
+      let xValues =
+        frame.[xKey]
+        |> Series.values
+      let mean = Statistics.Statistics.Mean xValues
+      let ssx = xValues |> Seq.sumBy (fun x -> pown (x - mean) 2)
+      if Seq.isEmpty otherKeys then
+        let sumOfSquaredXResiduals =
+          frame.[xKey] - (Stats.mean frame.[xKey])
+          |> Series.mapValues (fun x -> pown x 2)
+          |> Stats.sum
+        sqrt ((pown stdErr 2) / sumOfSquaredXResiduals)
+      else
+        let tolerance = computeToleranceFor xKey xKeys frame
+        stdErr / sqrt (ssx * tolerance)
+    // Pr(>|t|)
+    let private twoSidedtTest df tValue  =
+      let tValue = abs tValue
+      let rightTail = 1.0 - Distributions.StudentT.CDF(0.0, 1.0, df, tValue)
+      let leftTail = Distributions.StudentT.CDF(0.0, 1.0, df, -tValue)
+      leftTail + rightTail
+
+    let summary (fit:t<'a,'b> when 'b : comparison) =
+      let residuals = fit |> (residuals >> Series.sort >> Series.values >> Seq.toArray)
+      let fitted = fit |> (fittedValues >> Series.values >> Seq.toArray)
+      let actual = fit.InputFrame.[fit.yKey] |> Series.values |> Seq.toArray
+      let fiveVals = Statistics.SortedArrayStatistics.FiveNumberSummary residuals
+      let keys = ["Min:"; "1Q:"; "Median:"; "3Q"; "Max:"]
+      let namedFiveVals = Seq.zip keys fiveVals |> Series.ofObservations
+      let xtxInv = Fit.computeXtXinverse fit
+      let rhs = fit.Coefficients.Keys |> fun xs -> System.String.Join (" + ", xs)
+      let nObs = fit.InputFrame.[fit.yKey] |> Series.countKeys |> float
+      let nCoeffs = fit.Coefficients |> Series.countKeys
+      let r2 = GoodnessOfFit.RSquared (fitted, actual)
+      let stdErrorResiduals = GoodnessOfFit.StandardError (fitted, actual, nCoeffs)
+      let stdErrors =
+        fit.Coefficients
+        |> Series.map (fun key _ -> stdErrorResiduals * sqrt xtxInv.[key].[key])
+      let tValues = fit.Coefficients / stdErrors
+      let tTestProbs = tValues |> Series.mapValues (twoSidedtTest (nObs - float nCoeffs))
+      let tTable = [("Estimate:", fit.Coefficients); ("Std.Err.", stdErrors); ("t value", tValues); ("Pr(>|t|)", tTestProbs)] |> Frame.ofColumns
+      {
+        LinearFormula = fit.yKey.ToString() + " ~ " + rhs
+        ResidualFiveVals = namedFiveVals
+        TTable = tTable
+        RSquared = r2
+        AdjRSquared = 1.0 - (1.0 - r2) * (nObs - 1.0) / (nObs - float nCoeffs)
+      } : Summary.t<'b>
+
+  let private dataFrameContainsMissingValues columns df =
+    let noOfColumns = Set.count columns
+    Frame.countValues df
+    |> Series.foldValues (fun a x -> a && x = noOfColumns) true
+
+  let valuesToArray s = (Series.values >> Seq.toArray) s
+
+  let private fitWithOutIntercept xs y names =
+    let coefficients = Fit.multiDim false xs y
+    Seq.zip names coefficients
+    |> Series.ofObservations
+
+  let private fitWithIntercept xs y names interceptKey =
+    let coefficients = Fit.multiDim true xs y
+    let interceptKeyAsSeq = Seq.singleton interceptKey
+    let names' = Seq.append interceptKeyAsSeq names
+    Seq.zip names' coefficients
+    |> Series.ofObservations
+
+  let private multiFitCleanDataFrame xCols yCol fitIntercept df =
+    let xColArray = xCols |> Seq.toArray
+    let y = df |> Frame.getCol yCol |> valuesToArray
+    let rowToArray (series:ObjectSeries<'a>) =
+      xColArray
+      |> Array.map series.GetAs<float>
+    let xs =
+      df |> Frame.mapRowValues rowToArray |> Series.values |> Seq.toArray
+    match fitIntercept with
+    | None -> fitWithOutIntercept xs y xColArray
+    | Some interceptKey -> fitWithIntercept xs y xColArray interceptKey
+
+  let private interceptKeyIsEqualToColumnInDataFrame columns fitIntercept =
+    match fitIntercept with
+    | None -> false
+    | Some interceptKey -> Set.contains interceptKey columns
+
+  /// <summary>
+  /// Performs linear regression on the values in a dataframe.
+  /// </summary>
+  /// <param name="xCols">The column keys that constitutes the independent variables.</param>
+  /// <param name="yCol">The column key that consitutes the dependent variable.</param>
+  /// <param name="fitIntercept">An option type that specifies a key to use for the intercept in the result, if set to None, the fit will not produce an intercept.</param>
+  /// <param name="df">The dataframe to perform the regression on</param>
+  /// <returns>A series with column keys as keys, and regression coefficients as values.</returns>
+  let multiDim xCols yCol fitIntercept df =
+    let columns = xCols |> Set.ofSeq |> Set.add yCol
+    let df' = Frame.filterCols (fun k _ -> Set.contains k columns) df
+    if dataFrameContainsMissingValues columns df then
+      failwith "The dataframe you are attempting to do regression on contains missing values in the columns used for the regression. Consider dropping sparse rows."
+    if interceptKeyIsEqualToColumnInDataFrame columns fitIntercept then
+      failwith "The key specified for the intercept is equal to a column key in the dataframe, consider picking new key for your intercept or rename a column in the dataframe."
+    let coefficients = multiFitCleanDataFrame xCols yCol fitIntercept df'
+    Fit.make df coefficients yCol fitIntercept
+
+  /// <summary>
+  /// Performs a simple linear regression on two columns in the dataframe.
+  /// </summary>
+  /// <param name="xCol">The column key of the independent variable.</param>
+  /// <param name="yCol">The column key of the dependent variable.</param>
+  /// <param name="fitIntercept">An option type that specifies a key to use for the intercept in the result, if set to None, the fit will not produce an intercept.</param>
+  /// <param name="df">The dataframe to perform the regression on</param>
+  /// <returns>A series with the column keys of both the independent and dependent variable, and regression coefficients as values.</returns>
+  let simple xCol yCol fitIntercept df =
+    multiDim (Seq.singleton xCol) yCol fitIntercept df
+
---
+++ src/Deedle.Math/PCA.fs
@@ -0,0 +1,91 @@
+﻿namespace Deedle.Math
+
+module PCA =
+  open Deedle
+  open MathNet.Numerics.LinearAlgebra
+  open MathNet.Numerics.Statistics
+
+  type t<'a> when 'a : equality =
+    {
+      EigenVectors : Frame<'a, string>
+      EigenValues : Series<string, float>
+    }
+
+  /// <summary>
+  /// The eigen values of the PCA transformation.
+  /// </summary>
+  /// <param name="pca">The PCA transformation.</param>
+  let eigenValues pca =
+    pca.EigenValues
+
+  /// <summary>
+  /// The eigen vectors of the PCA transformation.
+  /// </summary>
+  /// <param name="pca">The PCA transformation.</param>
+  let eigenVectors pca =
+    pca.EigenVectors
+
+  let private normalizeSeriesUsing (mean : float) (stdDev : float) series =
+    let normalizeValue x =
+      (x - mean) / stdDev
+    series
+    |> Series.mapValues normalizeValue
+
+  let private normalizeSeries (series: Series<'a,float>) =
+    let mean = Stats.mean series
+    let stdDev = Stats.stdDev series
+    normalizeSeriesUsing mean stdDev series
+
+  /// <summary>
+  /// Normalizes the columns in the dataframe using a z-score.
+  /// That is (X - mean) / (std. dev)
+  /// </summary>
+  /// <param name="df">The dataframe to normalize</param>
+  let normalizeColumns (df:Frame<'a,'b>) =
+    let normalizeColumn (k:'b) (row:ObjectSeries<'a>) =
+      row.As<float>()
+      |> normalizeSeries
+    df
+    |> Frame.mapCols normalizeColumn
+
+
+  /// <summary>
+  /// Computes the principal components from the data frame.
+  /// The principal components are listed from PC1 .. PCn
+  /// Where PC1 explains most of the variance.
+  /// </summary>
+  /// <param name="dataFrame">A PCA datatype that contains the eigen values and vectors.</param>
+  let pca dataFrame =
+    let factorization =
+      dataFrame
+      |> Stats.covMatrix
+      |> Matrix.eigen
+
+    let createPcNameForIndex n =
+      sprintf "PC%d" (n + 1)
+
+    let colKeyArray = dataFrame.ColumnKeys |> Seq.toArray
+
+    let eigenValues =
+      // eigen values are returned with least significant first.
+      factorization.EigenValues
+      |> Vector.map (fun x -> x.Real)
+      |> Vector.toSeq
+      |> Seq.rev
+      |> Series.ofValues
+      |> Series.mapKeys createPcNameForIndex
+    let eigenVectors =
+      // as eigen vectors match the eigen values, these also has to be reversed.
+      factorization.EigenVectors
+      |> Matrix.toColSeq
+      |> Seq.rev
+      |> Seq.mapi (fun i x -> (createPcNameForIndex i, Vector.toSeq x |> Series.ofValues))
+      |> Frame.ofColumns
+      |> Frame.mapRowKeys (fun i -> colKeyArray.[i])
+
+    if eigenVectors.RowCount <> dataFrame.ColumnCount then
+      failwith "Row count of eigen vectors does not match the input columns"
+    {
+      EigenValues = eigenValues
+      EigenVectors = eigenVectors
+    }
